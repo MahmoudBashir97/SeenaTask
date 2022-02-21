c25f056ae25bf07d64536e526ba6c3afecdf556c
Mahmoud Bashir <anadeouf@yahoo.com>
2022-02-21 10:07:38 +0200

    Mahmoud , second commit , applying dataBinding on main activity_main.xml , and add some packages to be ready to use .

---
+++ app/src/main/java/com/mahmoudbashir/seenatask/ui/activities/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.mahmoudbashir.seenatask.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.mahmoudbashir.seenatask.R
+import com.mahmoudbashir.seenatask.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    }
+}
--- app/src/main/java/com/mahmoudbashir/seenatask/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/mahmoudbashir/seenatask/ui/fragments/HomeFragment.kt
@@ -1,25 +1,32 @@
-package com.mahmoudbashir.seenatask
+package com.mahmoudbashir.seenatask.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.mahmoudbashir.seenatask.R
+import com.mahmoudbashir.seenatask.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    lateinit var homeBinding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+       homeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+
+        return homeBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
     }
 }
